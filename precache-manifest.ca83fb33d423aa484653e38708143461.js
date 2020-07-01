99ec32b52c33259d0847f03e133f665f7ad5acd7
chrisva_co <chvc2003@hotmail.com>
2020-07-01 12:41:16 -0500

    Updates

---
+++ precache-manifest.ca83fb33d423aa484653e38708143461.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b189c1dc4eb63ff8da351d5dfdf48f56",
+    "url": "/gotter-react/index.html"
+  },
+  {
+    "revision": "fcfed4c9251ffc0aacf5",
+    "url": "/gotter-react/static/css/2.9f98d992.chunk.css"
+  },
+  {
+    "revision": "0ea30b9036e3e94a7402",
+    "url": "/gotter-react/static/css/main.0f761b9a.chunk.css"
+  },
+  {
+    "revision": "fcfed4c9251ffc0aacf5",
+    "url": "/gotter-react/static/js/2.35ce80df.chunk.js"
+  },
+  {
+    "revision": "8bb92c23487eb5c952e51992947cb95e",
+    "url": "/gotter-react/static/js/2.35ce80df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ea30b9036e3e94a7402",
+    "url": "/gotter-react/static/js/main.040f49d8.chunk.js"
+  },
+  {
+    "revision": "56f2c890e1a2d6e9cff6",
+    "url": "/gotter-react/static/js/runtime-main.2515083d.js"
+  },
+  {
+    "revision": "1769587d7e1330e9d4241c30f672c7ff",
+    "url": "/gotter-react/static/media/logo.1769587d.png"
+  },
+  {
+    "revision": "41f65f75cb0b41db6fe44ab4b074bacc",
+    "url": "/gotter-react/static/media/logow.41f65f75.png"
+  }
+]);
